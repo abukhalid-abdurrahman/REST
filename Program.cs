8f50e6f4640003411e92449873f933f25ac6fa50
faha-berdiev <madysoftware@gmail.com>
2020-04-30 02:09:01 +0300

    REST Начало изучения

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace REST_API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("REST API");
+        }
+    }
+}
