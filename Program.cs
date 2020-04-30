7a0bf3ce0f44dd9640caa6932e1231a23b6ecc09
faha-berdiev <madysoftware@gmail.com>
2020-04-30 18:35:38 +0300

    Класс REST

--- Program.cs
+++ Program.cs
@@ -1,42 +1,13 @@
 ﻿using System;
-using System.IO;
-using System.Net;
+using RESTAPI;
 
 namespace REST_API
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("REST API");
-            WebRequest request = WebRequest.Create("http://www.contoso.com/PostAccepter.aspx ");
-            request.Method = "POST";
-
-            string postData = "This is a test that posts this string to a Web server.";
-            byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(postData);
-
-            request.ContentType = "application/x-www-form-urlencoded";
-            request.ContentLength = byteArray.Length;
-
-            Stream dataStream = request.GetRequestStream();
-            dataStream.Write(byteArray, 0, byteArray.Length);
-            dataStream.Close();
-
-            WebResponse response = request.GetResponse();
-            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
-
-            using (dataStream = response.GetResponseStream())
-            {
-                StreamReader reader = new StreamReader(dataStream);
-                string responseFromServer = reader.ReadToEnd();
-                
-                StreamWriter streamWriter = new StreamWriter("index.html", false, System.Text.Encoding.UTF8);
-                streamWriter.Write(responseFromServer);
-                streamWriter.Flush();
-                streamWriter.Close();
-            }
-
-            response.Close();
         }
     }
 }
---
+++ REST.cs
@@ -0,0 +1,51 @@
+using System;
+using System.IO;
+using System.Net;
+
+namespace RESTAPI
+{
+    public class REST
+    {
+        public enum QueryTypeEnum
+        {
+            GET,
+            POST,
+            PUT,
+            PATCH,
+            DELETE
+        };
+        public string EndPoint;
+        public QueryTypeEnum QueryMethod;
+        public REST(string endPoint, QueryTypeEnum queryMethod)
+        {
+            this.EndPoint = endPoint;
+            this.QueryMethod = queryMethod;
+        }
+
+        public string SendRequest()
+        {
+            string responseValue = string.Empty;
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(EndPoint);
+            httpWebRequest.Method = QueryMethod.ToString();
+            using(HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse())
+            {
+                if(httpWebResponse.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new Exception();
+                }
+
+                using(Stream responseStreamReader = httpWebResponse.GetResponseStream())
+                {
+                    if(responseStreamReader != null)
+                    {
+                        using(StreamReader streamReader = new StreamReader(responseStreamReader))
+                        {
+                            responseValue = streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return responseValue;
+        }
+    }
+}
