41d9be95f352d380e74f80311bd1daeda5650ac4
faha-berdiev <madysoftware@gmail.com>
2020-05-05 01:48:31 +0300

    Конечная версия сборки REST

--- Program.cs
+++ Program.cs
@@ -8,15 +8,24 @@ namespace REST_API
         static void Main(string[] args)
         {
             string uriEndPoint = Tools.UserInterface.Input("Input URL: ", ConsoleColor.Green);
-            string authenticationType = Tools.UserInterface.Input("Input Authentication Type: ", ConsoleColor.Blue);
-            REST.AuthenticationTypeEnum authenticationTypeEnum = (REST.AuthenticationTypeEnum)Enum.Parse(typeof(REST.AuthenticationTypeEnum), authenticationType, true);
-            string techniqueType = Tools.UserInterface.Input("Input Technique Type: ", ConsoleColor.Blue);
-            REST.AutheticationTechniqueEnum autheticationTechniqueEnum = (REST.AutheticationTechniqueEnum)Enum.Parse(typeof(REST.AutheticationTechniqueEnum), techniqueType, true);
-            string userLogin = Tools.UserInterface.Input("Input Login: ", ConsoleColor.Red);
-            string userPassword = Tools.UserInterface.Input("Input Password: ", ConsoleColor.Red);
-            REST rest = new REST(uriEndPoint, REST.QueryTypeEnum.GET, userLogin, userPassword, authenticationTypeEnum, autheticationTechniqueEnum);
-            string restResponse = rest.SendRequest();
-            Tools.UserInterface.Output(restResponse, ConsoleColor.Green);
+            Console.Write("\n1. To Send Data\n2. To Read Data\nInput: ");
+            string restMethod = Console.ReadLine();
+            if(restMethod == "1")
+            {
+                Console.Write("Input Data: ");
+                string data = Console.ReadLine();
+                Console.Write("Input Content-Type: ");
+                string contentType = Console.ReadLine();
+                REST rest = new REST(uriEndPoint, REST.QueryTypeEnum.POST);
+                string restResponse = rest.WriteRequest(data, contentType);
+                Tools.UserInterface.Output(restResponse, ConsoleColor.Blue);
+            }
+            else if(restMethod == "2")
+            {
+                REST rest = new REST(uriEndPoint, REST.QueryTypeEnum.GET);
+                string restResponse = rest.ReadResponse();
+                Tools.UserInterface.Output(restResponse, ConsoleColor.Green);
+            }
         }
     }
 }
--- REST.cs
+++ REST.cs
@@ -15,48 +15,62 @@ namespace RESTAPI
             DELETE
         };
 
-        public enum AuthenticationTypeEnum
-        {
-            BASIC,
-            NTLM
-        };
-
-        public enum AutheticationTechniqueEnum
-        {
-            OWN,
-            NetworkCredential
-        }
         private string endPoint { get; set; }
-        private string userName { get; set; }
-        private string userPassword { get; set; }
         private QueryTypeEnum queryMethod;
-        private AutheticationTechniqueEnum autheticationTechnique;
-        private AuthenticationTypeEnum authenticationType;
         public REST(string endPoint, QueryTypeEnum queryMethod)
         {
             this.endPoint = endPoint;
             this.queryMethod = queryMethod;
         }
-
-        public REST(string endPoint, QueryTypeEnum queryMethod, string userName, string userPassword, AuthenticationTypeEnum authenticationType, AutheticationTechniqueEnum autheticationTechnique)
+        public string WriteRequest(string data, string contentType)
         {
-            this.endPoint = endPoint;
-            this.queryMethod = queryMethod;
-            this.userName = userName;
-            this.userPassword = userPassword;
-            this.authenticationType = authenticationType;
-            this.autheticationTechnique = autheticationTechnique;
+            WebRequest webRequest = (WebRequest)WebRequest.Create(endPoint);
+            WebResponse webResponse = null;
+
+            webRequest.Method = queryMethod.ToString();
+            webRequest.ContentType = contentType;
+
+            byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(data);
+            webRequest.ContentLength = byteArray.Length;
+
+            string responseFromServer = string.Empty;
+
+            try
+            {
+                Stream dataStream = webRequest.GetRequestStream();
+                dataStream.Write(byteArray, 0, byteArray.Length);
+                dataStream.Close();
+
+                webResponse = webRequest.GetResponse();
+
+                using (dataStream = webResponse.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(dataStream);
+                    responseFromServer = reader.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                responseFromServer = $"/error/{ex.Message}/";
+            }
+            finally
+            {
+                if (webResponse != null)
+                {
+                    ((IDisposable)webResponse).Dispose();
+                }
+            }
+
+            webResponse.Close();
+            return responseFromServer;
         }
-
-
-        public string SendRequestAutheticated()
+        public string ReadResponse()
         {
             string responseValue = string.Empty;
             HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(endPoint);
             httpWebRequest.Method = queryMethod.ToString();
-            string authHeaer = System.Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(userName + ":" + userPassword));
-            httpWebRequest.Headers.Add("Authorization", authenticationType.ToString() + " " + authHeaer);
             HttpWebResponse httpWebResponse = null;
+
             try
             {
                 using (httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse())
@@ -79,42 +93,7 @@ namespace RESTAPI
             }
             finally
             {
-                if(httpWebResponse != null)
-                {
-                    ((IDisposable)httpWebResponse).Dispose();
-                }
-            }
-            return responseValue;
-        }
-        public string SendRequest()
-        {
-            string responseValue = string.Empty;
-            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(endPoint);
-            httpWebRequest.Method = queryMethod.ToString();
-            HttpWebResponse httpWebResponse = null;
-            try
-            {
-                using (httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse())
-                {
-                    using (Stream responseStreamReader = httpWebResponse.GetResponseStream())
-                    {
-                        if (responseStreamReader != null)
-                        {
-                            using (StreamReader streamReader = new StreamReader(responseStreamReader))
-                            {
-                                responseValue = streamReader.ReadToEnd();
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                responseValue = $"/error/{ex.Message}/";
-            }
-            finally
-            {
-                if(httpWebResponse != null)
+                if (httpWebResponse != null)
                 {
                     ((IDisposable)httpWebResponse).Dispose();
                 }
